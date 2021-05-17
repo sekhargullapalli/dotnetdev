defc814a2214ee10b8502d77567d28e79e7a291b
Vijaya Sekhar Gullapalli <Vijaya.SekharGullapalli@swep.net>
2021-05-17 10:40:22 +0200

    jwt authentication added

--- asp.netcore/auth/vsg.ApiAuth/vsg.ApiAuth.JWT/Startup.cs
+++ asp.netcore/auth/vsg.ApiAuth/vsg.ApiAuth.JWT/Startup.cs
@@ -1,16 +1,21 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 using vsg.ApiAuth.JWT.Business;
@@ -29,7 +34,36 @@ namespace vsg.ApiAuth.JWT
         public void ConfigureServices(IServiceCollection services)
         {
             var jwtTokenConfig = Configuration.GetSection("jwtTokenConfig").Get<JwtTokenConfig>();
+            services.AddSingleton(jwtTokenConfig);
 
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x=> {
+
+                x.RequireHttpsMetadata = true; /*authentication requires HTTPS for the metadata address or authority*/
+                //the default value is true,
+                //which saves the JWT access token in the current HttpContext,
+                //so that we can retrieve it using the method
+                //await HttpContext.GetTokenAsync(“Bearer”, “access_token”) or something similar.
+                //If we want to set the SaveToken to be false,
+                //then we can save the JWT access token in claims,
+                //and then retrieve its value using the method:
+                //User.FindFirst("access_token")?.Value.
+               x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer=true,
+                    ValidIssuer=jwtTokenConfig.Issuer,
+                    ValidateIssuerSigningKey=true,
+                    IssuerSigningKey=new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtTokenConfig.Secret)),
+                    ValidAudience = jwtTokenConfig.Audience,
+                    ValidateAudience=true,
+                    ValidateLifetime=true,
+                    ClockSkew=TimeSpan.FromMinutes(1) /* allowed time difference (in seconds) between the server and client clocks when validating the exp and nbf claims*/
+                };
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -51,7 +85,7 @@ namespace vsg.ApiAuth.JWT
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
